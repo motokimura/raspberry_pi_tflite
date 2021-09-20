5ba15cc21066e6f06ac887a47b21fdd9a4d017b8
Motoki Kimura <motoki.kimura.1990@gmail.com>
2021-09-20 14:29:48 +0900

    add scripts

---
+++ README.md
@@ -0,0 +1,75 @@
+# raspberry_pi_tflite
+
+This repository is a slightly modified version of
+[TensorFlow Lite Python object detection example with Pi Camera](https://github.com/tensorflow/examples/tree/master/lite/examples/object_detection/raspberry_pi).
+
+An example to use [TensorFlow Lite](https://tensorflow.org/lite)
+on a Raspberry Pi to perform real-time object detection using images
+streamed from the Pi Camera. It draws a bounding box around each detected
+object in the camera preview (when the object score is above a given threshold).
+
+## Requirements
+
+- Raspberry Pi (tested with Raspberry Pi 4 model 4GB)
+- UbuntuOS (tested with Ubuntu Desktop 21.04)
+- Raspberry Pi Camera (test with V2 camera module)
+
+May work with other versions.
+
+## Setup your hardware
+
+Install UbuntuOS on your Raspberry Pi.
+
+You also need to [connect and configure the Pi Camera](
+https://www.raspberrypi.org/documentation/configuration/camera.md)..
+Since the image stream from the camera is processed by OpenCV `VideoCapture()`,
+USB cameras can also be used.
+
+## Install TensorFlow Lite runtime
+
+In this project, all you need from the TensorFlow Lite API is the `Interpreter`
+class. So instead of installing the large `tensorflow` package, we're using the
+much smaller `tflite_runtime` package.
+
+To install `tflite_runtime` package:
+
+```
+pip3 install --extra-index-url https://google-coral.github.io/py-repo/ tflite_runtime
+```
+
+If you use Rasberry Pi OS (OS other than Ubuntu), follow
+[Python quickstart](https://www.tensorflow.org/lite/guide/python#install_tensorflow_lite_for_python).
+
+You also have to install additional Python packages:
+
+```
+pip3 install -r requirements.txt
+```
+
+## Download the example files
+
+Use [TensorFlow Lite Python object detection example with Pi Camera script](https://github.com/tensorflow/examples/blob/master/lite/examples/object_detection/raspberry_pi/download.sh)
+to download the MobileNet model and labels file:
+
+```
+# The script takes an argument specifying where you want to save the model files
+bash download.sh /tmp
+```
+
+## Run the example
+
+```
+python3 detect.py \
+  --model /tmp/detect.tflite \
+  --labels /tmp/coco_labels.txt \
+  --thresh 0.4
+```
+
+You should see the camera feed appear on the monitor attached to your Raspberry
+Pi. Put some objects in front of the camera, like a coffee mug or keyboard, and
+you'll see boxes drawn around those that the model recognizes, including the
+label and score for each. It also prints the amount of time it took
+to perform each inference in milliseconds at the top-left corner of the screen.
+
+For more information about executing inferences with TensorFlow Lite, read
+[TensorFlow Lite inference](https://www.tensorflow.org/lite/guide/inference).
---
+++ detect.py
@@ -0,0 +1,140 @@
+# references
+# - https://github.com/tensorflow/examples/blob/master/lite/examples/object_detection/raspberry_pi/detect_picamera.py
+"""Example using TF Lite to detect objects with the Raspberry Pi camera."""
+
+from __future__ import absolute_import, division, print_function
+
+import argparse
+import re
+import time
+
+import cv2
+import numpy as np
+from PIL import Image
+from tflite_runtime.interpreter import Interpreter
+
+CAMERA_WIDTH = 640
+CAMERA_HEIGHT = 480
+
+
+def load_labels(path):
+    """Loads the labels file. Supports files with or without index numbers."""
+    with open(path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        labels = {}
+        for row_number, content in enumerate(lines):
+            pair = re.split(r'[:\s]+', content.strip(), maxsplit=1)
+            if len(pair) == 2 and pair[0].strip().isdigit():
+                labels[int(pair[0])] = pair[1].strip()
+            else:
+                labels[row_number] = pair[0].strip()
+    return labels
+
+
+def set_input_tensor(interpreter, image):
+    """Sets the input tensor."""
+    tensor_index = interpreter.get_input_details()[0]['index']
+    input_tensor = interpreter.tensor(tensor_index)()[0]
+    input_tensor[:, :] = image
+
+
+def get_output_tensor(interpreter, index):
+    """Returns the output tensor at the given index."""
+    output_details = interpreter.get_output_details()[index]
+    tensor = np.squeeze(interpreter.get_tensor(output_details['index']))
+    return tensor
+
+
+def detect_objects(interpreter, image, threshold):
+    """Returns a list of detection results, each a dictionary of object info."""
+    set_input_tensor(interpreter, image)
+    interpreter.invoke()
+
+    # Get all output details
+    boxes = get_output_tensor(interpreter, 0)
+    classes = get_output_tensor(interpreter, 1)
+    scores = get_output_tensor(interpreter, 2)
+    count = int(get_output_tensor(interpreter, 3))
+
+    results = []
+    for i in range(count):
+        if scores[i] >= threshold:
+            result = {
+                'bounding_box': boxes[i],
+                'class_id': classes[i],
+                'score': scores[i]
+            }
+            results.append(result)
+    return results
+
+
+def annotate_objects(cv2_frame, results, labels):
+    """Draws the bounding box and label for each object in the results."""
+    for obj in results:
+        # Convert the bounding box figures from relative coordinates
+        # to absolute coordinates based on the original resolution
+        ymin, xmin, ymax, xmax = obj['bounding_box']
+        xmin = int(xmin * CAMERA_WIDTH)
+        xmax = int(xmax * CAMERA_WIDTH)
+        ymin = int(ymin * CAMERA_HEIGHT)
+        ymax = int(ymax * CAMERA_HEIGHT)
+
+        # Overlay the box, label, and score on the camera preview
+        cv2_frame = cv2.rectangle(cv2_frame, (xmin, ymin), (xmax, ymax),
+                                  color=(0, 0, 255),
+                                  thickness=3)
+        cv2_frame = cv2.putText(
+            cv2_frame, '%s %.2f' % (labels[obj['class_id']], obj['score']),
+            (xmin + 5, ymin + 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255),
+            2)
+
+    return cv2_frame
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--model',
+                        help='File path of .tflite file.',
+                        required=True)
+    parser.add_argument('--labels',
+                        help='File path of labels file.',
+                        required=True)
+    parser.add_argument('--threshold',
+                        help='Score threshold for detected objects.',
+                        required=False,
+                        type=float,
+                        default=0.4)
+    args = parser.parse_args()
+
+    labels = load_labels(args.labels)
+    interpreter = Interpreter(args.model)
+    interpreter.allocate_tensors()
+    _, input_height, input_width, _ = interpreter.get_input_details(
+    )[0]['shape']
+
+    capture = cv2.VideoCapture(0)
+
+    while (True):
+        ret, cv2_frame = capture.read()
+        image = cv2.cvtColor(cv2_frame, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(image).resize((input_width, input_height),
+                                              Image.ANTIALIAS)
+
+        start_time = time.monotonic()
+        results = detect_objects(interpreter, image, args.threshold)
+        elapsed_ms = (time.monotonic() - start_time) * 1000
+
+        print(elapsed_ms, results)
+        cv2_frame = annotate_objects(cv2_frame, results, labels)
+
+        cv2.imshow('detection result', cv2_frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    capture.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
